80f69a2e1c04b24e984800ad2ca3300c1a27a009
Ezetorc <ezetorc@gmail.com>
2025-09-19 14:57:33 -0300

    Add sections

---
+++ src/utilities/adjustColor.ts
@@ -0,0 +1,16 @@
+export function adjustColor(hex: string, percent: number): string {
+  hex = hex.replace(/^#/, "");
+
+  let r = parseInt(hex.substring(0, 2), 16);
+  let g = parseInt(hex.substring(2, 4), 16);
+  let b = parseInt(hex.substring(4, 6), 16);
+
+  r = Math.min(255, Math.max(0, r + (r * percent) / 100));
+  g = Math.min(255, Math.max(0, g + (g * percent) / 100));
+  b = Math.min(255, Math.max(0, b + (b * percent) / 100));
+
+  return (
+    "#" +
+    [r, g, b].map((x) => Math.round(x).toString(16).padStart(2, "0")).join("")
+  );
+}
