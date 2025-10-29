04f723afa61f6e0bf02be739f473ce32bad020e9
Ezetorc <ezetorc@gmail.com>
2025-10-28 22:00:27 -0300

    Add static redirection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import { DEFAULT_LANGUAGE } from "./src/configuration/i18n.configuration";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  redirects: {
+    "/": `/${DEFAULT_LANGUAGE}`,
   },
 });
