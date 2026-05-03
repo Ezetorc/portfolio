a09ad33544a77d63eb03973b893ac5d45317404e
Ezetorc <ezetorc@gmail.com>
2026-05-03 18:41:30 -0300

    update last update date

--- src/configuration/general.configuration.ts
+++ src/configuration/general.configuration.ts
@@ -1,6 +1,6 @@
 import { calculateYearsSince } from "../utilities/calculate-years-since.utility";
 
-export const LAST_UPDATE_DATE = "02/11/2025";
+export const LAST_UPDATE_DATE = "05/03/2026";
 export const FULL_NAME = "Ezequías Torczuk";
 export const ENGLISH_LEVEL = "B2";
-export const YEARS_PROGRAMMING = calculateYearsSince(2023, 7, 9)
+export const YEARS_PROGRAMMING = calculateYearsSince(2023, 7, 9);
